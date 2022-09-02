68c204e674ef81dc739cdda949221d6e190ec099
Benjamin Yates-Rae <byatesrae@mx51.io>
2022-09-02 19:51:29 +1000

    Only run optional linters against new code

--- build/lint-optional.sh
+++ build/lint-optional.sh
@@ -5,7 +5,7 @@ set -e
 echo " * Linting (optional)..."
 echo
 
-$(go env GOPATH)/bin/golangci-lint run -c ./.golangci.optional.yml
+$(go env GOPATH)/bin/golangci-lint run -c ./.golangci.optional.yml -n --new-from-rev master
 
 echo
 echo " * Done."
