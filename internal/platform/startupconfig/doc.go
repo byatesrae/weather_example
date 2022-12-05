f9f3e32d8bfb1cbdfe32b736519a8d8519ed4dd6
Benjamin Yates-Rae <b.yatesrae@gmail.com>
2022-12-05 16:47:19 +1100

    Doco

--- internal/platform/startupconfig/doc.go
+++ internal/platform/startupconfig/doc.go
@@ -1,1 +1,3 @@
+// startupconfig combines flag & environment variable parsing with programmatic "Usage"
+// documentation detailing both approaches (as opposed to just flag usage).
 package startupconfig
